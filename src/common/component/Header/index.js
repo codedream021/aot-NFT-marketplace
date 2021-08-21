735da7d7dbaf86ba87f851a7899e1dd662503db0
jkadar186 <jkadar186@gmail.com>
2021-08-21 17:33:49 +0500

    Advance Search Page Completed

--- src/common/component/Header/index.js
+++ src/common/component/Header/index.js
@@ -1,25 +1,38 @@
 import React from 'react'
-import './style.css'
-
+import {Link} from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 
+import Profile from '../../../assets/profile_avatar.svg'
+import './style.css'
+
 const Header = () => {
+  const path = window.location.pathname
+
   return (
     <div className="header">
       <Container maxWidth="lg">
         <div className="header-inner">
           <AppBar position="static">
             <Toolbar>
-              <Typography variant="h6" className="logo" >
-                Aot Logo
-              </Typography>
-              <div>
+              <Link to="/">
+                <Typography variant="h6" className="logo" >
+                  Aot Logo
+                </Typography>
+              </Link>
+              <div className="right-bar">
                 <Button color="inherit">About AOT</Button>
-                <Button color="inherit">Login / Sign Up</Button>
+                {path !== '/' ? 
+                  <Button color="inherit">Login</Button>  
+                  :
+                  <Button color="inherit">Login / Sign Up</Button>
+                }
+                {path !== '/' &&
+                  <img className="avatar" src={Profile} alt="profile"/>
+                }
               </div>
             </Toolbar>
           </AppBar>
@@ -28,6 +41,5 @@ const Header = () => {
     </div>
   )
 }
-
 
 export default Header
